bbf0e03b395ebfc5fcbd5514d539a9f810cd6a1f
wusiquan <1781554387@qq.com>
2023-11-19 12:04:01 +0800

    ok

--- pages/test.vue
+++ pages/test.vue
@@ -8,7 +8,7 @@ async function test() {
   const r = await $fetch('/api/addLive', {
     method: 'POST',
     body: {
-      liveId: '670094'
+      liveId: '24953168'
     }
   })
   console.log('前端结果')
