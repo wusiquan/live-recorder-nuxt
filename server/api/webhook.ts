21c8986a485d762a906c8f96817f993700a8d54a
wusiquan <1781554387@qq.com>
2023-11-21 00:28:39 +0800

    ok

--- server/api/webhook.ts
+++ server/api/webhook.ts
@@ -1,8 +1,32 @@
+import * as crypto from 'crypto'
+
+const WEBHOOK_SECRET: string = 'ilovesirui001'
+
+const verifySignature = (body: any, signature256: string) => {
+  const signature = crypto
+    .createHmac("sha256", WEBHOOK_SECRET)
+    .update(JSON.stringify(body))
+    .digest("hex");
+  let trusted = Buffer.from(`sha256=${signature}`, 'ascii');
+  let untrusted =  Buffer.from(signature256, 'ascii');
+  return crypto.timingSafeEqual(trusted, untrusted);
+};
+
 export default defineEventHandler(async (event) => {
-  console.log(111, getRequestHeaders(event))
+  const headers = getRequestHeaders(event)
+  const body = readBody(event)
+  const signature256 = headers['x-hub-signature-256']
+
+  if (!signature256) {
+    setResponseStatus(event, 401)
+    send(event, 'Unauthorized')
+    return
+  }
+  
+  const result = verifySignature(body, signature256!)
 
   setResponseStatus(event, 201)
-  event.node.res.end('Accepted')
+  event.node.res.end(`haha ${result}`)
   
   console.log(222)
 })
