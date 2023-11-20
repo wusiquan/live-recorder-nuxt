a7cd01d702814202bcdab8494c4b501748c90419
wusiquan <1781554387@qq.com>
2023-11-20 22:53:10 +0800

    ok

--- server/api/webhook.ts
+++ server/api/webhook.ts
@@ -2,6 +2,7 @@ export default defineEventHandler(async (event) => {
   console.log(111, getRequestHeaders(event))
 
   setResponseStatus(event, 201)
-  // event.node.res.write('Accepted')
-  return 'Accepted'
+  event.node.res.end('Accepted')
+  
+  console.log(222)
 })
