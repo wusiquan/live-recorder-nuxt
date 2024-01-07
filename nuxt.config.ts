0f84b4ffa3dd93712d5deca5c5b1e8d316dd9fe6
wusiquan <1781554387@qq.com>
2024-01-07 19:06:00 +0800

    配置调整

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,7 @@
+// import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
@@ -37,12 +41,49 @@ export default defineNuxtConfig({
     // }
   },
 
+  // build: {
+  //   transpile:
+  //     process.env.NODE_ENV === 'production'
+  //       ? [
+  //           'naive-ui',
+  //           'vueuc',
+  //           '@css-render/vue3-ssr',
+  //           '@juggle/resize-observer'
+  //         ]
+  //       : ['@juggle/resize-observer']
+  // },
+
   vite: {
-    optimizeDeps: {
-      include:
-        process.env.NODE_ENV === 'development'
-          ? ['naive-ui']
-          : []
-    }
+    // ssr: {
+    //   noExternal: ['naive-ui'],
+    // },
+
+    plugins: [
+      // AutoImport({
+      //   imports: [
+      //     {
+      //       'naive-ui': [
+      //         'NSpace',
+      //         'NDataTable',
+      //         'NTag',
+      //         'NButton'
+      //       ]
+      //     }
+      //   ]
+      // }),
+      Components({
+        dts: true,
+        resolvers: [
+          NaiveUiResolver()
+        ],
+      })
+    ],
+
+    // optimizeDeps: {
+    //   include:
+    //     process.env.NODE_ENV === 'development'
+    //       ? ['naive-ui', 'vueuc', 'date-fns-tz/formatInTimeZone']
+    //       : []
+    // }
   }
 })
